449d30bd893f30aad5fe583c79f1a89d15f35c37
Exequiel <exequielac@correo.ugr.es>
2021-12-20 20:53:09 +0100

    Ejer 1 hecho

---
+++ p3VC.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Exequiel Alberto Castro Rivero
+DNI: 78308513E
+"""
+
+#########################################################################
+############ CARGAR LAS LIBRERÍAS NECESARIAS ############################
+#########################################################################
+
+# Terminar de rellenar este bloque con lo que vaya haciendo falta
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+import gc
+
+# Importar librerías necesarias
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import tensorflow.keras.utils as np_utils
+
+# Importar modelos y capas específicas que se van a usar
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+
+# Importar el optimizador a usar
+from tensorflow.keras.optimizers import SGD
+
+# Importar el conjunto de datos
+from tensorflow.keras.datasets import cifar100
+
+
+#########################################################################
+######## FUNCIÓN PARA CARGAR Y MODIFICAR EL CONJUNTO DE DATOS ###########
+#########################################################################
+
+# A esta función sólo se le llama una vez. Devuelve 4 vectores conteniendo,
+# por este orden, las imágenes de entrenamiento, las clases de las imágenes
+# de entrenamiento, las imágenes del conjunto de test y las clases del
+# conjunto de test.
+
+def cargarImagenes():
+  # Cargamos Cifar100. Cada imagen tiene tamaño (32, 32, 3).
+  # Nos vamos a quedar con las imágenes de 25 de las clases.
+  
+  (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
+  x_train = x_train.astype('float32')
+  x_test = x_test.astype('float32')
+  x_train /= 255
+  x_test /= 255
+  
+  train_idx = np.isin(y_train, np.arange(25))
+  train_idx = np.reshape(train_idx,-1)
+  x_train = x_train[train_idx]
+  y_train = y_train[train_idx]
+  
+  test_idx = np.isin(y_test, np.arange(25))
+  test_idx = np.reshape(test_idx, -1)
+  x_test = x_test[test_idx]
+  y_test = y_test[test_idx]
+  
+  # Transformamos los vectores de clases en matrices. Cada componente se convierte en un vector
+  # de ceros con un uno en la componente correspondiente a la clase a la que pertenece la imagen.
+  # Este paso es necesario para la clasificación multiclase en keras.
+  y_train = np_utils.to_categorical(y_train, 25)
+  y_test = np_utils.to_categorical(y_test, 25)
+  
+  return x_train, y_train, x_test, y_test
+
+#########################################################################
+######## FUNCIÓN PARA OBTENER EL ACCURACY DEL CONJUNTO DE TEST ##########
+#########################################################################
+
+# Esta función devuelve el accuracy de un modelo, definido como el 
+# porcentaje de etiquetas bien predichas frente al total de etiquetas.
+# Como parámetros es necesario pasarle el vector de etiquetas verdaderas
+# y el vector de etiquetas predichas, en el formato de keras (matrices
+# donde cada etiqueta ocupa una fila, con un 1 en la posición de la clase
+# a la que pertenece y 0 en las demás).
+
+def calcularAccuracy(labels, preds):
+  labels = np.argmax(labels, axis = 1)
+  preds = np.argmax(preds, axis = 1)
+  
+  accuracy = sum(labels == preds)/len(labels)
+  
+  return accuracy
+
+#########################################################################
+## FUNCIÓN PARA PINTAR LA PÉRDIDA Y EL ACCURACY EN TRAIN Y VALIDACIÓN ###
+#########################################################################
+
+# Esta función pinta dos gráficas, una con la evolución de la función
+# de pérdida en el conjunto de train y en el de validación, y otra
+# con la evolución del accuracy en el conjunto de train y en el de
+# validación. Es necesario pasarle como parámetro el historial
+# del entrenamiento del modelo (lo que devuelven las funciones
+# fit() y fit_generator()).
+
+def mostrarEvolucion(hist):
+
+  loss = hist.history['loss']
+  val_loss = hist.history['val_loss']
+  plt.plot(loss)
+  plt.plot(val_loss)
+  plt.legend(['Training loss', 'Validation loss'])
+  plt.show()
+
+  acc = hist.history['accuracy']
+  val_acc = hist.history['val_accuracy']
+  plt.plot(acc)
+  plt.plot(val_acc)
+  plt.legend(['Training accuracy', 'Validation accuracy'])
+  plt.show()
+  
+
+# EJERCICIO 1
+  
+x_train, y_train, x_test, y_test = cargarImagenes()
+
+#########################################################################
+################## DEFINICIÓN DEL MODELO BASENET ########################
+#########################################################################
+
+input_shape = (32, 32, 3)
+num_classes = 25
+
+model = Sequential()
+model.add(Conv2D(6, kernel_size=(5, 5),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(16, kernel_size=(5, 5),
+                 activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(50, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+
+# A completar
+# 1.- Incluir  import del tipo de modelo y capas a usar
+# 2.- definir model e incluir las capas en él
+
+#########################################################################
+######### DEFINICIÓN DEL OPTIMIZADOR Y COMPILACIÓN DEL MODELO ###########
+#########################################################################
+
+model.compile(loss=tf.keras.losses.categorical_crossentropy,
+              optimizer=tf.keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+
+# Una vez tenemos el modelo base, y antes de entrenar, vamos a guardar los 
+# pesos aleatorios con los que empieza la red, para poder reestablecerlos 
+# después y comparar resultados entre no usar mejoras y sí usarlas.
+weights = model.get_weights()
+
+#########################################################################
+###################### ENTRENAMIENTO DEL MODELO #########################
+#########################################################################
+
+hist = model.fit(x_train, y_train,
+          batch_size=256,
+          epochs=100,
+          verbose=1,
+          validation_split=0.1)
+
+mostrarEvolucion(hist)
+
+#########################################################################
+################ PREDICCIÓN SOBRE EL CONJUNTO DE TEST ###################
+#########################################################################
+
+#Incluir model.eva.luate() 
+#Incluir función que muestre la perdida y accuracy del test
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+del model
+del hist
+del tf
+gc.collect()
+
+
+#########################################################################
+########################## MEJORA DEL MODELO ############################
+#########################################################################
+
+# A completar. Tanto la normalización de los datos como el data
+# augmentation debe hacerse con la clase ImageDataGenerator.
+# Se recomienda ir entrenando con cada paso para comprobar
+# en qué grado mejora cada uno de ellos.
+
